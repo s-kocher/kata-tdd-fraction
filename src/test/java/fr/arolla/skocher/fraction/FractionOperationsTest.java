0aa852a56232d2a6e747fd7d540017439de2f8ac
Sylvain K <skocher@avb202.com>
2021-05-05 15:14:00 +0200

    Red 2/5 + 2/5 = 4/5

--- src/test/java/fr/arolla/skocher/fraction/FractionOperationsTest.java
+++ src/test/java/fr/arolla/skocher/fraction/FractionOperationsTest.java
@@ -83,4 +83,14 @@ public class FractionOperationsTest {
         assertEquals(new FractionVo(3, 5), result);
     }
 
+    @Test
+    public void should_sum_2_5_plus_2_5_be_4_5() {
+        FractionVo fraction1 = new FractionVo(2, 5);
+        FractionVo fraction2 = new FractionVo(2, 5);
+
+        FractionVo result = FractionOperations.sum(fraction1, fraction2);
+
+        assertEquals(new FractionVo(4, 5), result);
+    }
+
 }
