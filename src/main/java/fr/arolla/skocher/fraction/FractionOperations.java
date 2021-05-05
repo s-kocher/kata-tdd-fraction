96a589762e7eb52ef746a4dc6f1ef887f67ccdd5
Sylvain K <sylvain.kocher@avb202.com>
2021-05-05 09:38:27 +0200

    Add FractionVo and gcd method

--- src/main/java/fr/arolla/skocher/fraction/FractionOperations.java
+++ src/main/java/fr/arolla/skocher/fraction/FractionOperations.java
@@ -2,4 +2,8 @@ package fr.arolla.skocher.fraction;
 
 public class FractionOperations {
 
+    static int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
 }
---
+++ src/main/java/fr/arolla/skocher/fraction/FractionVo.java
@@ -0,0 +1,36 @@
+package fr.arolla.skocher.fraction;
+
+import java.util.Objects;
+
+public class FractionVo {
+
+    private final int numerator;
+    private final int denominator;
+
+    public FractionVo(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FractionVo that = (FractionVo) o;
+        return numerator == that.numerator && denominator == that.denominator;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numerator, denominator);
+    }
+
+}
