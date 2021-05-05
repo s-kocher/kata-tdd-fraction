a4e9ae65540f056acfb92bcb1f230c92d1c7429e
Sylvain K <skocher@avb202.com>
2021-05-05 14:49:03 +0200

    Add 2 tests to control the equals of FractionVo
    Equals method of the Fraction Value Object generated by IDE so it's more a test in case it must evolves later...

--- src/test/java/fr/arolla/skocher/fraction/FractionOperationsTest.java
+++ src/test/java/fr/arolla/skocher/fraction/FractionOperationsTest.java
@@ -1,9 +1,26 @@
 package fr.arolla.skocher.fraction;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class FractionOperationsTest {
+
+    @Test
+    public void should_two_same_fractions_be_equal() {
+        FractionVo fraction1 = new FractionVo(1, 3);
+        FractionVo fraction2 = new FractionVo(1, 3);
+
+        assertEquals(fraction1, fraction2);
+    }
+
+    @Test
+    public void should_two_different_fractions_be_not_equal() {
+        FractionVo fraction1 = new FractionVo(1, 3);
+        FractionVo fraction2 = new FractionVo(1, 4);
+
+        assertNotEquals(fraction1, fraction2);
+    }
 
 }
