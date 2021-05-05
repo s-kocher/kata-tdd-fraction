aa72fb08020e79a8fcf7c2992798448c0ca5d5dd
Sylvain K <skocher@avb202.com>
2021-05-05 16:06:02 +0200

    Refactor : generic ways to get the numerator with different denominator

--- src/main/java/fr/arolla/skocher/fraction/FractionOperations.java
+++ src/main/java/fr/arolla/skocher/fraction/FractionOperations.java
@@ -8,17 +8,9 @@ public class FractionOperations {
 
     public static FractionVo sum(FractionVo fraction1, FractionVo fraction2) {
         if (fraction1.getDenominator() != fraction2.getDenominator()) {
-            int numerator;
-            if (fraction1.getNumerator() == 1 && fraction1.getDenominator() == 5 && fraction2.getNumerator() == 2 && fraction2.getDenominator() == 11) {
-                numerator = 21;
-            }  else if (fraction1.getNumerator() == 1 && fraction1.getDenominator() == 6 && fraction2.getNumerator() == 2 && fraction2.getDenominator() == 11) {
-                numerator = 23;
-            }  else if (fraction1.getNumerator() == 1 && fraction1.getDenominator() == 6 && fraction2.getNumerator() == 3 && fraction2.getDenominator() == 11) {
-                numerator = 29;
-            }  else {
-                numerator = fraction1.getDenominator() + fraction2.getDenominator();
-            }
-
+            int numerator =
+                fraction1.getNumerator() * fraction2.getDenominator() +
+                fraction2.getNumerator() * fraction1.getDenominator();
             int denominator = fraction1.getDenominator() * fraction2.getDenominator();
 
             return new FractionVo(numerator, denominator);
